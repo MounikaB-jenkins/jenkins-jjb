27c4122abc6b367ba38f2c9716b5044ce1dbe406
EC2 Default User <ec2-user@ip-172-31-2-137.ap-south-2.compute.internal>
2026-07-10 18:03:49 +0000

    banking app jobs using JJB

---
+++ test-jjb.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Get the JSON secret
+SECRET=$(aws secretsmanager get-secret-value \
+  --secret-id jenkins-apitoken \
+  --query SecretString \
+  --output text)
+
+# Extract username and token
+USER=$(echo "$SECRET" | jq -r '.username')
+TOKEN=$(echo "$SECRET" | jq -r '.token')
+
+# Generate jenkins_jobs.ini
+cat > jenkins_jobs.ini <<EOF
+[job_builder]
+ignore_cache=True
+keep_descriptions=False
+recursive=True
+
+[jenkins]
+url=https://my-jenkins-lab.duckdns.org/
+user=$USER
+password=$TOKEN
+query_plugins_info=False
+EOF
+
+echo "===== Generated jenkins_jobs.ini ====="
+cat jenkins_jobs.ini
+echo "======================================"
+
+# Test JJB
+jenkins-jobs --conf jenkins_jobs.ini test jobs
